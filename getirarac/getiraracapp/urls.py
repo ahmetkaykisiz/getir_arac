0be89223046506aa68da765196b1f4179b1a25dc
AHMET KAYKISIZ <36927325+ahmetkaykisiz@users.noreply.github.com>
2024-02-28 15:28:42 +0300

    Ga 4 import fix 5 (#10)
    * GA-4 general fix-3
    - edited api urls for ngnix
    - edited views name
    
    * GA-4 general fix-5
    - fixed allowed host

--- getirarac/getiraracapp/urls.py
+++ getirarac/getiraracapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GetDriverList
 
 urlpatterns = [
-    path('getir_arac/get_user_list/', GetDriverList.as_view(), name='item-list'),
+    path('get_user_list/', GetDriverList.as_view(), name='item-list'),
 ]
